63e658e04e7f9b221e5d8bcc8132e7e6c516194e
Christo du Toit <christo@dutoit.co.uk>
2023-04-11 15:20:17 +0100

    ShouldReturnValidationSummaryIfExceptionAndInnerExceptionHasDataItems -> PASS

--- Xeption.Tests/XeptionExtensionTests.Logic.GetValidationSummary.cs
+++ Xeption.Tests/XeptionExtensionTests.Logic.GetValidationSummary.cs
@@ -67,5 +67,36 @@ namespace Xeptions.Tests
             // then
             actualResult.Should().BeEquivalentTo(expectedResult);
         }
+
+        [Fact]
+        public void ShouldReturnValidationSummaryIfExceptionAndInnerExceptionHasDataItems()
+        {
+            // given
+            string randomMessage = GetRandomString();
+            string randomInnerExceptionMessage = GetRandomString();
+            Xeption randomInnerXeption =
+                new Xeption(message: randomInnerExceptionMessage);
+
+            randomInnerXeption.Data.Add("Error3", new List<string> { "Error message 4" });
+            randomInnerXeption.Data.Add("Error4", new List<string> { "Error message 5", "Error message 6" });
+
+            Xeption randomXeption =
+                new Xeption(randomMessage, randomInnerXeption);
+
+            randomXeption.Data.Add("Error1", new List<string> { "Error message 1" });
+            randomXeption.Data.Add("Error2", new List<string> { "Error message 2", "Error message 3" });
+
+            string expectedResult =
+                $"{randomXeption.GetType().Name} Errors:  " +
+                $"Error1 => Error message 1;  Error2 => Error message 2, Error message 3;  \r\n" +
+                $"{randomInnerXeption.GetType().Name} Errors:  " +
+                $"Error3 => Error message 4;  Error4 => Error message 5, Error message 6;  \r\n";
+
+            // when
+            string actualResult = randomXeption.GetValidationSummary();
+
+            // then
+            actualResult.Should().BeEquivalentTo(expectedResult);
+        }
     }
 }
