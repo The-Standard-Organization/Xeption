1b69b4ecd8d778c20b4c171d87caa675ff389898
Christo du Toit <christo@dutoit.co.uk>
2024-07-19 17:45:51 +0100

    CODE RUB: Code cleanup

--- Xeption/XeptionExtensions.cs
+++ Xeption/XeptionExtensions.cs
@@ -42,7 +42,7 @@ namespace Xeptions
             catch (Exception error)
             {
                 message = error.Message;
-
+                
                 return false;
             }
         }
